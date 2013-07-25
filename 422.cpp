afc1a8f17573c5be3f287ccea0dd0ea419d8f097
Gao Bicheng <volz.kz.g@gmail.com>
2013-07-25 10:26:19 +0800

    Thinking

---
+++ 422.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int n,t;
+
+int main()
+{
+        ios::sync_with_stdio(false);
+        
+}
