705c48d7fcadeeba2edb3b48b8bfb5fee0b61e73
Gao Bicheng <volz.kz.g@gmail.com>
2013-09-12 17:05:36 +0800

    Test

--- 422.cpp
+++ 422.cpp
@@ -5,13 +5,44 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
-int n,t;
+const int MAX = 3333;
+double p[MAX],f[MAX][MAX],t,now;
+int n;
+
+void init()
+{
+        cin >> n >> t;
+        for (int i = 1; i <= n; ++i) cin >> p[i];
+}
+
+void solve()
+{
+        for (int y = 1; y <= n; ++y) 
+                for (int x = y-1; x >= 0; --x) {
+                        now = (double)1 - p[x+1];
+                        f[x][y] = (y - x + t) + (y - x) * p[x+1];
+                        for (int z = x + 2; z <= y; ++z) {
+                                f[x][y] += now * p[z] * (y - z + 1 + f[z-1][y]);
+                                now *= ((double)1 - p[z]);
+                        }
+                        f[x][y] /= ((double)1-p[x+1]);
+                }
+        cout << setiosflags(ios::fixed)
+                << setprecision(8) 
+                << f[0][n] << endl;
+}
 
 int main()
 {
+        freopen("test.in","r",stdin);
         ios::sync_with_stdio(false);
-        
+
+        init();
+        solve();
+
+        return 0;
 }
