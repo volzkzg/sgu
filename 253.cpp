7957f48f32f3d1868fde129b6c428100bf0cea97
GaoBicheng <volz.kz.g@gmail.com>
2014-10-14 12:41:54 +0800

    check 253.cpp

--- 253.cpp
+++ 253.cpp
@@ -19,50 +19,48 @@ int n,m,k;
 
 double det(point p,point q)
 {
-    return p.x*q.y-p.y*q.x;
+	return p.x*q.y-p.y*q.x;
 }
+
 void theIniter()
 {
-    cin >> n >> m >> k;
-    rep(i,1,n) cin >> p[i].x >> p[i].y;
-    rep(i,2,n) p[i]-=p[1];
+	cin >> n >> m >> k;
+	rep(i,1,n) cin >> p[i].x >> p[i].y;
+	rep(i,2,n) p[i]-=p[1];
 }
+
 void theSolver()
 {
-    rep(i,1,m)
-	{
-	    cin >> P.x >> P.y;
-	    P-=p[1];
-	    if (det(p[2],P)<0 || det(p[n],P)>0) continue;
-	    int l = 3,r = n;
-	    while (l<=r)
-		{
-		    int mid = (l+r)>>1;
-		    double d1 = det(p[mid],P);
-		    double d2 = det(p[mid-1],P);
-		    if (d1<=0)
-			{
-			    if (d2>=0)
-				{
-				    if (det(P-p[mid-1],p[mid]-p[mid-1])<=0)
-					k--;
-				    break;
+	rep(i,1,m) {
+		cin >> P.x >> P.y;
+		P-=p[1];
+		if (det(p[2],P)<0 || det(p[n],P)>0) continue;
+		int l = 3,r = n;
+		while (l<=r) {
+
+			int mid = (l+r)>>1;
+			double d1 = det(p[mid],P);
+			double d2 = det(p[mid-1],P);
+			if (d1<=0) {
+				if (d2>=0) {
+					if (det(P-p[mid-1],p[mid]-p[mid-1])<=0)
+						k--;
+					break;
 				}
-			    else r = mid-1;
+				else r = mid-1;
 			}
-		    else
-			l = mid+1;
+			else
+				l = mid+1;
 		}
 
 	}
-    if (k<=0)cout << "YES" << endl;
-    else cout << "NO" << endl;
+	if (k<=0)cout << "YES" << endl;
+	else cout << "NO" << endl;
 }
+
 int main()
 {
-    freopen("test.in","r",stdin);
-    freopen("test.out","w",stdout);
-    ios::sync_with_stdio(false);
-    theIniter();
-    theSolver();
+	ios::sync_with_stdio(false);
+	theIniter();
+	theSolver();
 }
